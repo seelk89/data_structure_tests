68431934e62ce5941fb19e41ea7d8c4f7df9eee2
seelk89 <seelk.89@gmail.com>
2020-05-26 20:30:32 +0200

    json read

--- json_tests.py
+++ json_tests.py
@@ -35,7 +35,21 @@ for i in range(1000):
     with open('test.json', 'w') as f:
         json.dump(data, f)
 
-print('Write time: ', timeit.default_timer() - start)
+print('Write time: ', (timeit.default_timer() - start) / 1000)
 
+start = timeit.default_timer()
+for i in range(1000):
+    with open('test.json') as f:
+        data = json.load(f)
 
-print('Read time: ')
+    rn = random.randint(1, 3)
+    if rn == 1:
+        print(data['depth1'][random.randint(1, len(data['depth1']) - 1)])
+
+    if rn == 2:
+        print(data['depth1'][0]['depth2'][random.randint(1, len(data['depth1'][0]['depth2']) - 1)])
+
+    if rn == 3:
+        print(data['depth1'][0]['depth2'][0]['depth3'][random.randint(1, len(data['depth1'][0]['depth2'][0]['depth3']) - 1)])
+
+print('Read time: ', (timeit.default_timer() - start) / 1000)
